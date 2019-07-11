28addf69b4c07a1a92657fd7f3a01d28efba8fb4
marcustung <marcustung@boyu66.net>
2019-07-11 21:32:14 +0800

    First Version

---
+++ NETCoreWebAPIVersioning/NetCoreWebAPIVersion/NetCoreWebAPIVersion/Controllers/Values2Controller.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreWebAPIVersion.ControllersV2
+{
+    [ApiController]
+    [ApiVersion("0.1", Deprecated =true)]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class Values3Controller : ControllerBase
+    {
+        [HttpGet]
+        public string Get(ApiVersion apiVersion) => $"Controller = {GetType().Name}\nVersion = {apiVersion}";
+    }
+
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(ApiVersion apiVersion) => $"Controller = {GetType().Name}\nVersion = {apiVersion}";
+    }
+
+    [ApiController]
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/values")]
+    public class Values2Controller : ControllerBase
+    {
+        [HttpGet]
+        public string Get(ApiVersion apiVersion) => $"Controller = {GetType().Name}\nVersion = {apiVersion}";
+    }
+
+    [ApiVersionNeutral]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        public string Get() => "OK";
+    }
+}
